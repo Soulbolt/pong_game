acb7894e2d084a3422703305fd488aa2c8811edf
Soulbolt <soulbolt@gmail.com>
2023-12-16 01:20:20 -0700

    Created functions for ball collision detetion for top and bottom boundaries and both paddles. Reset ball to center and move towards opposite player when respetive paddles miss.

--- ball.py
+++ ball.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 
+
 class Ball(Turtle):
 
     def __init__(self):
@@ -7,9 +8,19 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
-        # self.goto(350, 400)
+        self.x_move = 10
+        self.y_move = 10
 
     def move(self):
-        self.setheading(25)
-        self.goto(self.xcor() + 25, self.ycor() + 20)
+        self.goto(self.xcor() + self.x_move, self.ycor() + self.y_move)
+
+    def bounce_y(self):
+        self.y_move *= -1  # to turn number negative for opposite direction
+
+    def bounce_x(self):
+        self.x_move *= -1
+
+    def reset_position(self):
+        self.goto(0,0)
+        self.bounce_x()
 
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import time
 from turtle import Screen
 from paddle import Paddle
+from ball import Ball
 
 screen = Screen()
 screen.title("Pong")
@@ -10,6 +11,7 @@ screen.tracer(0)  # Needed along with update and time.sleep using while loop
 
 player_1 = Paddle((-350, 0))
 player_2 = Paddle((350, 0))
+ball = Ball()
 
 screen.listen()
 screen.onkey(player_1.move_up, "w")
@@ -20,8 +22,22 @@ screen.onkey(player_2.move_down, "Down")
 game_is_on = True
 
 while game_is_on:
-    screen.update()
     time.sleep(0.1)
+    screen.update()
+
+    ball.move()
+    # Detect Collision with top and bottom walls
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    # Detect Collision with both paddles
+    if ball.distance(player_2) < 50 and ball.xcor() > 320 or ball.distance(player_1) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+    # Detect when right paddle misses
+    if ball.xcor() > 400:
+        ball.reset_position()
+    # Detect when left paddle misses
+    if ball.xcor() < -400:
+        ball.reset_position()
 
     # for dist in range(0, 10, 20):
     #     if player_2.ycor() == 0:
