5dfe1b9b0c09438adf8248b706bc0a803eeefc30
Soulbolt <soulbolt@gmail.com>
2023-12-16 00:19:40 -0700

    refactored code for simplicity, display paddles on each side, each with their respective movement keys. commented out code WIP

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ screen.bgcolor("black")
 screen.setup(width=800, height=600)
 screen.tracer(0)  # Needed along with update and time.sleep using while loop
 
-paddle = Paddle()
-player_1 = paddle.player_one
-player_2 = paddle.player_two
+player_1 = Paddle((-350, 0))
+player_2 = Paddle((350, 0))
 
 screen.listen()
-screen.onkey(paddle.move_up, "Up")
-screen.onkey(paddle.move_down, "Down")
+screen.onkey(player_1.move_up, "w")
+screen.onkey(player_1.move_down, "s")
+screen.onkey(player_2.move_up, "Up")
+screen.onkey(player_2.move_down, "Down")
 
 game_is_on = True
 
@@ -22,14 +23,14 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
 
-    for dist in range(0, 10, 20):
-        if player_2.ycor() == 0:
-            paddle.cpu_move_up()
-    player_2.ycor()
-
-    for dist in range(0, 10, 20):
-        if player_2.ycor() > -260 or player_2.ycor() == 260:
-            paddle.cpu_move_down()
-    player_2.ycor()
+    # for dist in range(0, 10, 20):
+    #     if player_2.ycor() == 0:
+    #         paddle.cpu_move_up()
+    # player_2.ycor()
+    #
+    # for dist in range(0, 10, 20):
+    #     if player_2.ycor() > -260 or player_2.ycor() == 260:
+    #         paddle.cpu_move_down()
+    # player_2.ycor()
 
 screen.exitonclick()
