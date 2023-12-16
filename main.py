d1e7ad2b7bd59af89a3a3d72f9e4a8db0132d07d
Soulbolt <soulbolt@gmail.com>
2023-12-15 17:14:45 -0700

    Added listener and functions to move up or down when using arrow keys respectively. move paddle to the right side of the screen and added title.

--- main.py
+++ main.py
@@ -1,22 +1,31 @@
 from turtle import Turtle, Screen
 
+screen = Screen()
+screen.title("Pong")
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+
 paddle = Turtle()
 paddle.color("white")
 paddle.penup()
-paddle.goto(-780,0)
+paddle.goto(350, 0)
 paddle.shape("square")
 paddle.shapesize(stretch_len=1, stretch_wid=5)
 
-screen = Screen()
-screen.bgcolor("black")
-screen.screensize(canvwidth=800, canvheight=600)
-
-
 def move_up():
-    paddle.setheading(90)
+    new_y = paddle.ycor() + 20
+    paddle.goto(350, new_y)
+
+
+def move_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(350, new_y)
+
 
 
 screen.listen()
+screen.onkey(move_up,"Up")
+screen.onkey(move_down,"Down")
 
 
 
